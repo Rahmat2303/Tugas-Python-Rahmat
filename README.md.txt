94c760018d06bed6f1c75f5035deb8496d2cdc81
Rahmat <rahmat.hidayat2826@gmail.com>
2020-11-08 07:43:23 +0700

    file terbaru

---
+++ README.md.txt
@@ -0,0 +1,10 @@
+Tugas 1
+	
+	Jadi pada tugas pertama menampilkan angka secara berurutan 
+mulai dari 0 ke samping sampai angka 9 dan 0 kebawah sampai angka 9 
+sehingga membentuk urutan angka bebentuk kotak. Dengan menggunakan range.
+
+Tugas 2
+
+	Jadi pada tugas kedua ini menampilkan angka yang lebih kecil
+dari 0.5 dengan menggunakan while.
---
+++ tugas.py
@@ -0,0 +1,23 @@
+
+# Tugas 1
+print(list(range(10)))
+print(list(range(1, 11)))
+print(list(range(2, 12)))
+print(list(range(3, 13)))
+print(list(range(4, 14)))
+print(list(range(5, 15)))
+print(list(range(6, 16)))
+print(list(range(7, 17)))
+print(list(range(8, 18)))
+print(list(range(9, 19)))
+
+
+# Tugas 2
+count = 0.18718606083294542
+count = 0.05883021198263216
+count = 0.46641338196131765
+count = 0.10053309734320115
+count = 0.4757932520466899
+while (count < 0.5):
+    print(count, "kurang dari 0.5")
+    break
